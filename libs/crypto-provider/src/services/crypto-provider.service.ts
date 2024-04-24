94697f4c2f38c9d59092e739ac614c72df20811e
Ryan Ribeiro <jooownyy@gmail.com>
2024-04-24 20:59:55 +0000

    feat(crypto-provider): getProvider return the correct class based on input

--- libs/crypto-provider/src/services/crypto-provider.service.ts
+++ libs/crypto-provider/src/services/crypto-provider.service.ts
@@ -1,29 +1,41 @@
 import { Currency, CurrencyNetwork } from '@app/shared';
-import { Injectable } from '@nestjs/common';
-import { CryptoProvider } from '../providers/crypto.provider';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import {
   BtcCryptoProvider,
   EthCryptoProvider,
   LtcCryptoProvider,
 } from '../providers';
+import { ProviderNotFoundException } from '../exceptions/provider-not-found.exception';
 
-const providers: {
-  [K in Currency]: (network: CurrencyNetwork<K>) => CryptoProvider<K>;
-} = {
-  BTC: (network) => new BtcCryptoProvider(network),
-  LTC: (network) => new LtcCryptoProvider(network),
-  ETH: (network) => new EthCryptoProvider(network),
-} as const;
+type CryptoProvider<T extends Currency> = T extends 'BTC'
+  ? BtcCryptoProvider
+  : T extends 'ETH'
+    ? EthCryptoProvider
+    : T extends 'LTC'
+      ? LtcCryptoProvider
+      : never;
 
 @Injectable()
-export class CryptoProviderService {
+export class CryptoProviderService implements OnModuleInit {
+  private providers: Readonly<{
+    [K in Currency]: (network: CurrencyNetwork<K>) => CryptoProvider<K>;
+  }>;
+
+  public onModuleInit() {
+    this.providers = Object.freeze({
+      BTC: (network) => new BtcCryptoProvider(network),
+      LTC: (network) => new LtcCryptoProvider(network),
+      ETH: (network) => new EthCryptoProvider(network),
+    });
+  }
+
   public getProvider<T extends Currency>(
     currency: T,
     network: CurrencyNetwork<T>,
   ): CryptoProvider<T> {
-    const provider = providers[currency];
+    const provider = this.providers[currency];
     if (!provider) {
-      throw new Error(`Provider for ${currency} not found`);
+      throw new ProviderNotFoundException(currency);
     }
 
     return provider(network);
